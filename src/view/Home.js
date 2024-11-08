9e8a5b7a664c48fa962e2735f34dc95c241538c7
Matt Day <mattday@matts-mac-mini.lan>
2024-11-07 19:18:40 -0500

    Added About Paragraph

--- src/view/Home.js
+++ src/view/Home.js
@@ -10,7 +10,7 @@ import InstagramEmbed from '../components/Instagram.js'
 import ContactForm from '../components/ContactForm.js'
 
 
-export default function Home({info}) {
+export default function Home({ info }) {
     const [zoom, setZoom] = useState(1)
 
     const scrollToTop = () => {
@@ -72,15 +72,28 @@ export default function Home({info}) {
                 <img class="about-IMG" src={Collage1} />
                 <div class="about-info">
                     <h1>Our Story</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, dolore
-                        libero sapiente rerum similique soluta odit vitae natus amet ipsa velit
-                        cupiditate sunt nulla sint ex in? Est, et temporibus.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, dolore
-                        libero sapiente rerum similique soluta odit vitae natus amet ipsa velit
-                        cupiditate sunt nulla sint ex in? Est, et temporibus.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, dolore
-                        libero sapiente rerum similique soluta odit vitae natus amet ipsa velit
-                        cupiditate sunt nulla sint ex in? Est, et temporibus.</p>
+                    <article className='about-paragraph'>
+                        <p>Founded in 1961, Triangle Asphalt Paving has been a mainstay of Boone County for over sixty years.
+                            With only two dump trucks and an asphalt distributor, founder Jack Day and two original partners
+                            (hence “Triangle”) built the company from the ground up.  Beginning with patch work and chip seal,
+                            Jack and Triangle quickly expanded and, by 1964, were paving the streets of Lebanon, IN.  </p>
+                        <p>Current president Steve Day stepped into a leadership role in 1975 and his brother John Day joined
+                            the team in 1980.  Throughout the 70s, Triangle operated two asphalt plants, three paving crews,
+                            and twenty dump trucks until scaling back in 1980.  They successfully ran their HMA plants until
+                            1990 and 1999 respectively before selling them off and leaving highway work behind to focus on
+                            city work and lower-volume roads around the Boone County area.  In the early 2000s, as warehouses
+                            grew out of the cornfields, Triangle was right there as one of the leaders in laying large
+                            area parking lots.  </p>
+                        <p>Even after Jack’s passing in 2000, Triangle remained a family-owned-and-operated business with Steve
+                            as president, John as vice-president (until his passing in June 2024), and their older sister
+                            Elizabeth “Libby” Lewis running the Lebanon office until her retirement in 2016.  Every one of
+                            Jack’s five grandchildren has worked for Triangle in some capacity with multiple still employed
+                            by the company today.  </p>
+                        <p>If you talk to almost anyone in Lebanon, IN, you can guarantee they know someone connected to Triangle
+                            Asphalt.  Whether it’s sponsoring a little league team or donating to local organizations, they are
+                            a pillar of the community.  With an unmatched commitment to excellence, Triangle is the best in the
+                            asphalt business. </p>
+                    </article>
                 </div>
             </div>
             <div id="SJ">
